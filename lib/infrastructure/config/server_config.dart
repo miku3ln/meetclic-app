47e38fcce755d1ca33f3ad8c64257e6f53c5c3be
Alex alba <alexalbaa@cne.gob.ec>
2025-07-17 17:07:31 -0500

    ready management

--- lib/infrastructure/config/server_config.dart
+++ lib/infrastructure/config/server_config.dart
@@ -4,5 +4,5 @@
 
 // infrastructure/config/server_config.dart
 class ServerConfig {
-  static const String baseUrl = 'https://localhost/meetclickmanager/api';
+  static const String baseUrl = 'http://192.168.137.1/meetclickmanager/api';
 }
