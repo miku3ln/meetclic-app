02265482a8b0676075363b5d0ef5775a3e35ef1a
Alex alba <alexalbaa@cne.gob.ec>
2025-08-22 11:14:26 -0500

    read

--- lib/infrastructure/config/server_config.dart
+++ lib/infrastructure/config/server_config.dart
@@ -6,16 +6,17 @@
 enum Environment { production, developer, test, local }
 
 abstract class Config {
-  static const socket = '192.168.137.1';
-  static const port = 3000;
+  static const socket = '185.28.23.139';
+  static const port = 3001;
 }
 
 class ServerConfig {
   // static const String baseUrl = 'http://192.168.137.1/meetclickmanager/api';
   //static const String baseUrl = 'http://192.168.0.101/meetclickmanager/api';
-  static Environment currentEnv = Environment.local;
+  static Environment currentEnv = Environment.production;
   static String get getSocketServer {
-    return 'ws://${Config.socket}:${Config.port}/audio';
+    //return 'ws://${Config.socket}:${Config.port}/audio';
+    return 'ws://${Config.socket}/socketMigu3ln/audio';
   }
 
   static String get baseUrl {
