ee5eb8ecb8f76e3bb60d165f663655f3aec0bb8c
miku3ln <kalexmiguelalba@gmail.com>
2025-11-07 09:07:19 -0500

    ra

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+
 import 'app/init_mock_app.dart';
 import 'domain/services/session_service.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   WidgetsBinding.instance.renderView.automaticSystemUiAdjustment = false;
